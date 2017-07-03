6b68d89cf9d772f90cd7a572066527bfdf13e9c2
MichalCiesliczka <michal.ciesliczka@xsolve.com>
2017-07-03 15:45:41 +0200

    Fix typo

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import sum from './sum';
 var totalSum = sum(5, 3);
 
 console.log('Sum of 5 and 3 = ' + totalSum);
-console.log('Sum of 5 and 3 = ' + totalSum * 2);
+console.log('Double sum of 5 and 3 = ' + totalSum * 2);
